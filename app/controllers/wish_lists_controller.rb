73474ebc42a699cef2aef8f052467e731873c3b4
amk <a2772@mail.ru>
2013-01-22 22:21:10 +0200

    zayavka with edit-wishlist

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -36,7 +36,23 @@ class WishListsController < ApplicationController
   def edit
     @wish_list = WishList.find(params[:id])
   end
+   #--------------------------take obj_id from wishlist and set them like main
+   #noinspection RubyEmptyRescueBlockInspection
+  def setMainWantAttribute (w_id)
 
+    w_rec=Want.find(w_id)
+    wishlists=w_rec.wish_list
+    wishlists.each do |item|
+        w_rec.obj_id      = item['field_value']   if item['field_name'] =='obj_id'
+        w_rec.room        = item['field_value']    if item['field_name'] =='room'
+        w_rec.price_want  = item['field_value']    if item['field_name'] =='price_want'
+        w_rec.variant     = item['field_value']    if item['field_name'] =='variant'
+        w_rec.save()
+
+    end
+  rescue
+    logger.info "setMainWantAttr - error w_id="+w_id.to_s
+  end
   # POST /wish_lists
   # POST /wish_lists.json
   def create
@@ -45,11 +61,13 @@ class WishListsController < ApplicationController
     @wish_list.delete("action")
     @wish_list.delete("controller")
     @wish_list.delete("wish_list")
+
+    #----save wishlist-----------------------------
     @wish_list.each do |key,item|
       if item.blank?
         next
       end
-      if item["id"].blank? #---added--
+      if item["id"].blank? #---added-------------------------------
         if item["want_id"].blank?
            if @want_id.blank?
                #---need add new ec to want
@@ -59,34 +77,30 @@ class WishListsController < ApplicationController
            end
            item["want_id"]=@want_id
            item.delete("zayavka_id")
+        else
+          @want_id=item["want_id"]
         end
-
+        item.delete("zayavka_id")
         @wl_rec=WishList.new(item)
         if not @wl_rec.save()
           render  json: @wish_list.errors, status: :unprocessable_entity
         end
-      else
+      else  #----update------------------------------------------------
         @wl_rec=WishList.find(item["id"])
         if not @wl_rec.update_attributes(item)
            render  json: @wish_list.errors, status: :unprocessable_entity
         end
       end
-
+      @want_id=item["want_id"]
     end
+    logger.info "setMainWantAttr  w_id="+@want_id.to_s
+    self.setMainWantAttribute(@want_id) if not @want_id.blank?
     #render :text=>@wish_list.inspect
     #return
     respond_to do |format|
       format.json { head :no_content }
     end
-    #respond_to do |format|
-    #  if @wish_list.save
-    #    format.html { redirect_to @wish_list, notice: 'Wish list was successfully created.' }
-    #    format.json { render json: @wish_list, status: :created, location: @wish_list }
-    #  else
-    #    format.html { render action: "new" }
-    #    format.json { render json: @wish_list.errors, status: :unprocessable_entity }
-    #  end
-    #end
+
   end
 
   # PUT /wish_lists/1
