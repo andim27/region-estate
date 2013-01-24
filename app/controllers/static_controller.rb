689091571df417e84137a266842a65a1d392ee29
amk <a2772@mail.ru>
2013-01-24 23:53:55 +0200

    zayavka with edit-wishlist

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,11 @@
 class StaticController < ApplicationController
    def index
      #render :text=>"Hello static"
+     @dop_params_rows = DopParam.select("id,_parentId,name,fields_list").all
+     @obj_rows=Obj.select("id,name").all.to_json.html_safe
+     @total_rows      = @dop_params_rows.size
+     @dop_params_rows = @dop_params_rows.to_json.html_safe
+
      render :template=>'admin/_dop_params_crud.html',:layout =>false
    end
 end
