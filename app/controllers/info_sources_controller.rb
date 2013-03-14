faae4b3d4fbff7501e09ecb64e3cc60d1650c858
amk <a2772@mail.ru>
2013-03-14 16:19:44 +0200

    zayavka with edit-wishlist

--- app/controllers/info_sources_controller.rb
+++ app/controllers/info_sources_controller.rb
@@ -1,4 +1,6 @@
 class InfoSourcesController < ApplicationController
+
+  MAX_SESSION_TIME = 60 * 60 #--my--s
   # GET /info_sources
   # GET /info_sources.json
   def index
@@ -61,11 +63,17 @@ class InfoSourcesController < ApplicationController
     @info_source = InfoSource.find(params[:id])
 
     respond_to do |format|
-      if @info_source.update_attributes(params[:info_source])
-        format.html { redirect_to @info_source, notice: 'Info source was successfully updated.' }
+     # if @info_source.update_attributes(params[:info_source])
+      params.delete(:action)
+      params.delete(:controller)
+      #
+      #render :text=>params.inspect
+      #return
+      if @info_source.update_attributes(params)
+        # format.html { redirect_to @info_source, notice: 'Info source was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        # format.html { render action: "edit" }
         format.json { render json: @info_source.errors, status: :unprocessable_entity }
       end
     end
@@ -81,5 +89,30 @@ class InfoSourcesController < ApplicationController
       format.html { redirect_to info_sources_url }
       format.json { head :no_content }
     end
+  end
+
+  # parser
+  # '{"total":"11","rows":[{"id":"255004","firstname":"test","lastname":"blubber","phone":"33333333","email":""},{"id":"255006","firstname":"juan","lastname":"lopez","phone":"2251","email":""},{"id":"255009","firstname":"lkj","lastname":"lkj","phone":"lkj","email":"asdf@asd.com"},{"id":"255014","firstname":"SASDFF","lastname":"SFSDAA","phone":"","email":""},{"id":"255016","firstname":"cvxcb","lastname":"dfgdf","phone":"5454","email":"dgdf@de.de"},{"id":"255017","firstname":"sfsdfsd","lastname":"sdfsd","phone":"dds","email":"dgdf@de.de"},{"id":"255018","firstname":"sdfsd","lastname":"dsfsdf","phone":"fsd","email":"dgdf@de.de"},{"id":"255019","firstname":"fsdfssdf","lastname":"sdfsd","phone":"sdfsd","email":"dgdf@de.de"},{"id":"255020","firstname":"qsdqs","lastname":"qsdqs","phone":"qsdqsd","email":"dgdf@de.de"},{"id":"255021","firstname":"dsqd","lastname":"qsdqs","phone":"qsdqs","email":"dgdf@de.de"}]}'
+  def get_parsed_list
+     res=Hash.new
+     ##items=InfoSource.select("DISTINCT info_sources.*,ipr.id as info_sources_id ").where("info_url <>''").joins("LEFT JOIN info_parser_rules as ipr ON ipr.id_info_source=info_sources.id").page(params[:page]).per(params[:rows])
+     infoS=InfoSource
+     infoS=infoS.select("info_sources.*")
+     infoS=infoS.where("info_type = ?",params[:info_type]) if params[:info_type].to_i != 0
+     infoS=infoS.where("info_url <>''")
+     res[:total]=infoS.size ##InfoSource.where("info_url <>''").size
+     items=infoS.page(params[:page]).per(params[:rows])
+     #--items=infoS.select("info_sources.*").where("info_url <>''").page(params[:page]).per(params[:rows])
+
+     res[:rows]=items
+
+     if !session[:expiry_time].nil? and session[:expiry_time] < Time.now
+       # Session has expired. Clear the current session.
+       reset_session
+     end
+     # Assign a new expiry time, whether the session has expired or not.
+     session[:expiry_time] = MAX_SESSION_TIME.seconds.from_now
+
+     render :json=>res
   end
 end
